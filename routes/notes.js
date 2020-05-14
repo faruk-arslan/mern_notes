dc61ae890ed93ec3d3c7dc2839e22c79e3823424
Faruk Arslan <farukarsfa@gmail.com>
2020-05-15 01:38:50 +0300

    Complete CRUD back-end

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const notesController = require('../controllers/notesController');
 
-router.get('/', notesController.index)
+router.get('/', notesController.index);
+router.get('/all', notesController.getNotes);
+router.post('/add', notesController.addNote);
+router.put('/update', notesController.updateNote);
+router.delete('/delete', notesController.deleteNote);
+
 
 // router.get('/',(req,res)=>{
 //     res.send("Notes route indec path");
